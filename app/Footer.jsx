f8d22b78b317a621fbdbcf0f288d72fd0b99d080
int128 <int128@gmail.com>
2015-04-25 00:17:13 +0900

    Change footer to a link for screen shot automation

--- app/Footer.jsx
+++ app/Footer.jsx
@@ -1,10 +1,25 @@
 var React = require('react');
 
 module.exports = React.createClass({
+  getInitialState: function () {
+    return {clicked: 1};
+  },
+  handleClick: function () {
+    if (this.state.clicked >= 3) {
+      this.props.enableDemo();
+    } else {
+      this.setState({clicked: this.state.clicked + 1});
+    }
+    return false;
+  },
   render: function () {
     var manifest = require('../static/manifest.json');
-    return <p className="footer" onDoubleClick={this.props.enableDemo}>
-      {manifest.name} {manifest.version}
-    </p>;
+    return (
+      <div className="footer">
+        <a href="#" onClick={this.handleClick}>
+          {manifest.name} {manifest.version}
+        </a>
+      </div>
+    );
   }
 });
