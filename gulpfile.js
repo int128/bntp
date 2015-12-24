4604d9e0fa1082083df1f926770356d1be3c54a8
int128 <int128@gmail.com>
2015-12-24 15:43:18 +0900

    Rebuild gulpfile with ES6

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,48 @@
-require('coffee-script/register');
-require('./gulpfile.coffee');
+const gulp    = require('gulp');
+const webpack = require('gulp-webpack');
+const uglify  = require('gulp-uglify');
+const zip     = require('gulp-zip');
+const del     = require('del');
+
+gulp.task('build', ['webpack', 'vendor', 'static']);
+
+gulp.task('webpack', () =>
+  gulp.src('app/main.jsx')
+    .pipe(webpack({
+      output: { filename: 'main.js' },
+      externals: { react: 'React' },
+      module: {
+        loaders: [
+          { test: /\.jsx$/, loader: 'babel', query: { presets: ['es2015', 'react'] }},
+          { test: /\.json$/, loader: 'json-loader' },
+          { test: /\.less$/, loader: 'style/useable!css!less' }
+        ]
+      }
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest('build/extension')));
+
+gulp.task('vendor', () =>
+  gulp.src('node_modules/react/dist/react.min.js')
+    .pipe(gulp.dest('build/extension')));
+
+gulp.task('static', () =>
+  gulp.src('static/**/*')
+    .pipe(gulp.dest('build/extension')));
+
+
+gulp.task('watch', ['clean'], () => {
+  gulp.start('build');
+  gulp.watch('app/**/*', ['webpack']);
+  gulp.watch('static/**/*', ['static']);
+});
+
+gulp.task('default', ['clean'], () =>
+  gulp.start('build'));
+
+gulp.task('clean', (cb) => del('build/', cb));
+
+gulp.task('zip', () =>
+  gulp.src('build/extension/**/*')
+    .pipe(zip('extension.zip'))
+    .pipe(gulp.dest('build/')));
