d01f2d6af04de1de0b41402927360c1a5b0c4cbb
int128 <int128@gmail.com>
2016-01-03 18:28:20 +0900

    Refactor

--- app/view/PrefsVisibility.jsx
+++ app/view/PrefsVisibility.jsx
@@ -3,35 +3,33 @@ import React from 'react';
 export default class extends React.Component {
   render() {
     return (
-      <section>
+      <form>
         <p>Toggle</p>
-        <Components
-          showTopSites={this.props.showTopSites}
-          showBookmarks={this.props.showBookmarks}
-          onChange={this.props.onChange}
-          />
-      </section>
+        <Visibility
+          name="showTopSites"
+          title="Top Sites"
+          value={this.props.showTopSites}
+          onChange={this.props.onChange}/>
+        <Visibility
+          name="showBookmarks"
+          title="Bookmarks"
+          value={this.props.showBookmarks}
+          onChange={this.props.onChange}/>
+      </form>
     );
   }
 }
 
-class Components extends React.Component {
+class Visibility extends React.Component {
   render() {
     return (
-      <form>
-        <label>
-          <input type="checkbox"
-            checked={this.props.showTopSites}
-            onChange={(e) => this.props.onChange('showTopSites', e.target.checked)}/>
-          Top Sites
-        </label>
-        <label>
-          <input type="checkbox"
-            checked={this.props.showBookmarks}
-            onChange={(e) => this.props.onChange('showBookmarks', e.target.checked)}/>
-          Bookmarks
-        </label>
-      </form>
+      <label>
+        <input type="checkbox"
+          name={this.props.name}
+          checked={this.props.value}
+          onChange={(e) => this.props.onChange(this.props.name, e.target.checked)}/>
+        {this.props.title}
+      </label>
     );
   }
 }
