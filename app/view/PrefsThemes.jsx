5facc2eaacc67297066c071454b85753a44dc98f
int128 <int128@gmail.com>
2016-01-03 18:55:28 +0900

    Refactor

--- app/view/PrefsThemes.jsx
+++ app/view/PrefsThemes.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import ThemeItems from '../repository/Themes.jsx';
+import Themes from '../repository/Themes.jsx';
 
 export default class extends React.Component {
   constructor(props) {
@@ -8,39 +8,42 @@ export default class extends React.Component {
     this.state = {value: this.props.themeName};
   }
   componentDidMount() {
-    ThemeItems.findOrDefault(this.state.value).enable();
+    Themes.findOrDefault(this.state.value).enable();
   }
   onChange(currentValue) {
     const previousValue = this.state.value;
-    ThemeItems.findOrDefault(previousValue).disable();
-    ThemeItems.findOrDefault(currentValue).enable();
+    Themes.findOrDefault(previousValue).disable();
+    Themes.findOrDefault(currentValue).enable();
     this.setState({value: currentValue});
     this.props.onChange(currentValue);
   }
   render() {
     return (
-      <section>
+      <form>
         <p>Themes</p>
-        <ThemeForm items={ThemeItems.all()} defaultValue={this.state.value}
-          onChange={this.onChange.bind(this)}/>
-      </section>
+        {Themes.all().map(theme =>
+          <Theme
+            name={theme.name}
+            title={theme.title}
+            value={this.state.value}
+            onChange={this.onChange.bind(this)}/>
+        )}
+      </form>
     );
   }
 }
 
-class ThemeForm extends React.Component {
+class Theme extends React.Component {
   render() {
     return (
-      <form className="Themes">
-        {this.props.items.map(item =>
-          <label>
-            <input type="radio" name="Theme" value={item.name}
-              defaultChecked={item.name == this.props.defaultValue}
-              onChange={(e) => this.props.onChange(e.target.value)}/>
-            {item.title}
-          </label>
-        )}
-      </form>
+      <label>
+        <input type="radio"
+          name="Theme"
+          value={this.props.name}
+          defaultChecked={this.props.name == this.props.value}
+          onChange={(e) => this.props.onChange(e.target.value)}/>
+        {this.props.title}
+      </label>
     );
   }
 }
