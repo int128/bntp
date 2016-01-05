fbffe37c48826359c3c2d22f56b3c408aac586dd
int128 <int128@gmail.com>
2016-01-05 21:38:22 +0900

    Add link to this extension on webstore

--- app/view/Footer.jsx
+++ app/view/Footer.jsx
@@ -4,9 +4,10 @@ import Manifest from '../../static/manifest.json';
 
 export default class extends React.Component {
   render() {
+    const store = `https://chrome.google.com/webstore/detail/${chrome.runtime.id}`;
     return (
       <section className="Footer">
-        <p>{Manifest.name} {Manifest.version}</p>
+        <p><a href={store}>{Manifest.name}</a> {Manifest.version}</p>
       </section>
     );
   }
