5d70c8a1673b7a326c961b93d2144e5cc055c821
Hidetake Iwata <int128@gmail.com>
2017-05-30 22:23:29 +0900

    Extract initial state

--- src/index.js
+++ src/index.js
@@ -3,21 +3,13 @@ import { render } from 'react-dom';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 
-import reducers from './state/reducers';
-import listeners from './state/listeners';
-
-import Manifest from './infrastructure/Manifest';
 import ListenerMiddleware from './infrastructure/ListenerMiddleware';
 
-import RootContainer from './components/RootContainer';
+import reducers from './state/reducers';
+import listeners from './state/listeners';
+import initialState from './state/initialState';
 
-import {
-  chromePageRepository,
-  folderPreferenceRepository,
-  themeRepository,
-  themePreferenceRepository,
-  visibilityRepository,
-} from './repositories';
+import RootContainer from './components/RootContainer';
 
 import './index.css';
 
@@ -25,23 +17,6 @@ const devMiddlewares = [];
 if (process.env.NODE_ENV === 'development') {
   devMiddlewares.push(require('redux-logger')());
 }
-
-const initialState = () => {
-  const chromePageFolders = chromePageRepository.findFolders();
-  const folderPreference = folderPreferenceRepository.get();
-  const selectedTheme = themePreferenceRepository.getOrDefault();
-  const themes = themeRepository.findAll();
-  const visibilities = visibilityRepository.findAll();
-  const manifest = Manifest.get();
-  return {
-    chromePageFolders,
-    folderPreference,
-    selectedTheme,
-    themes,
-    visibilities,
-    manifest,
-  };
-};
 
 const store = createStore(
   combineReducers(reducers),
