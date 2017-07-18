818a67776fdaf81661b1318469657f636a0555c4
Hidetake Iwata <int128@gmail.com>
2017-07-18 16:52:58 +0900

    Build production release on tag push

--- scripts/github-release.sh
+++ scripts/github-release.sh
@@ -1,23 +1,28 @@
 #!/bin/bash -xe
-if [ "$TRAVIS_TAG" ]; then
-  export ARTIFACT="extension.zip"
-  test -f "$ARTIFACT"
 
-  curl -L -o ~/github-release.tar.bz2 https://github.com/aktau/github-release/releases/download/v0.7.2/linux-amd64-github-release.tar.bz2
-  tar -C ~ -jxf ~/github-release.tar.bz2
-  export PATH=~/bin/linux/amd64:"$PATH"
+test "$TRAVIS_TAG"
+sed -i -e "s/SNAPSHOT/$TRAVIS_TAG/g" public/manifest.json
+npm run clean
+npm run build
+npm run zip
 
-  github-release release \
-    --user int128 \
-    --repo bntp \
-    --tag "$TRAVIS_TAG" \
-    --name "$TRAVIS_TAG" \
-    --description "Released on $(date +%Y-%m-%d)"
+export ARTIFACT="extension.zip"
+test -f "$ARTIFACT"
 
-  github-release upload \
-    --user int128 \
-    --repo bntp \
-    --tag "$TRAVIS_TAG" \
-    --name "$(basename "$ARTIFACT")" \
-    --file "$ARTIFACT"
-fi
+curl -L -o ~/github-release.tar.bz2 https://github.com/aktau/github-release/releases/download/v0.7.2/linux-amd64-github-release.tar.bz2
+tar -C ~ -jxf ~/github-release.tar.bz2
+export PATH=~/bin/linux/amd64:"$PATH"
+
+github-release release \
+  --user int128 \
+  --repo bntp \
+  --tag "$TRAVIS_TAG" \
+  --name "$TRAVIS_TAG" \
+  --description "Released on $(date +%Y-%m-%d)"
+
+github-release upload \
+  --user int128 \
+  --repo bntp \
+  --tag "$TRAVIS_TAG" \
+  --name "$(basename "$ARTIFACT")" \
+  --file "$ARTIFACT"
