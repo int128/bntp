1ce1565c6e54066bf9a3d7b8c537f395b3a92e9a
Hidetake Iwata <int128@gmail.com>
2020-11-29 14:43:10 +0900

    Show top sites

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const topSites = useTopSites();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="TopSites">
+        <ul>
+          {topSites.map((e, i) => <li key={i}><a href={e.url}>{e.title}</a></li>)}
+        </ul>
+      </div>
     </div>
   );
+}
+
+function useTopSites() {
+  const [topSites, setTopSites] = useState<chrome.topSites.MostVisitedURL[]>([]);
+  useEffect(() => {
+    chrome.topSites.get(results => {
+      setTopSites(results);
+    });
+  }, []);
+  return topSites;
 }
 
 export default App;
