bc25905703e51e2f570d0ce81d4dbf8a1f76f9a5
Hidetake Iwata <int128@gmail.com>
2020-12-06 22:01:12 +0900

    Refactor: extract custom hook

--- v3/src/components/Themes.tsx
+++ v3/src/components/Themes.tsx
@@ -4,8 +4,8 @@ import './Themes.css';
 
 export function Themes() {
   const THEMES = ['light', 'dark', 'solarized-light', 'solarized-dark'];
-  const [selectedTheme, setSelectedTheme] = useState(THEMES[0]);
 
+  const [selectedTheme, setSelectedTheme] = useLocalStorage('SelectedTheme', THEMES[0]);
   useEffect(() => {
     document.documentElement.className = `Theme__${selectedTheme}`;
   });
@@ -23,4 +23,31 @@ export function Themes() {
       )}
     </div>
   );
+}
+
+function useLocalStorage(localStorageKey: string, initialValue: string): [string, (value: string) => void] {
+  const [storedValue, setStoredValue] = useState<string>(() => {
+    const value = localStorage.getItem(localStorageKey);
+    if (value === null) {
+      return initialValue;
+    }
+    return value;
+  });
+
+  useEffect(() => {
+    function handleStorageEvent(e: StorageEvent) {
+      if (e.storageArea === localStorage && e.key === localStorageKey && e.newValue !== null) {
+        setStoredValue(e.newValue);
+      }
+    }
+    window.addEventListener('storage', handleStorageEvent);
+    return () => {
+      window.removeEventListener('storage', handleStorageEvent);
+    };
+  }, [setStoredValue, localStorageKey]);
+
+  return [storedValue, (value: string) => {
+    setStoredValue(value);
+    localStorage.setItem(localStorageKey, value);
+  }];
 }
