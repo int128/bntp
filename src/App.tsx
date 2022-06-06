4ee4f59fa0e9359c9758c6f8dd4c880498a3b648
Hidetake Iwata <int128@gmail.com>
2022-06-06 19:30:20 +0900

    Migrate src

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { BookmarkFolders } from './Bookmarks/component';
+import { Preferences } from './Preferences/component';
+import { TopSites } from './TopSites/component';
 
-export default App;
+export const App = () =>
+  <div className="App">
+    <TopSites />
+    <BookmarkFolders />
+    <Preferences />
+  </div>
