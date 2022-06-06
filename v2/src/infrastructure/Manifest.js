cd6d40f3505f357d760603412be7ac2932fcccfb
Hidetake Iwata <int128@gmail.com>
2022-06-06 10:56:51 +0900

    Delete v2 directory (#20)
    * Delete v2 directory
    
    * Update README.md

--- v2/src/infrastructure/Manifest.js
+++
@@ -1,7 +0,0 @@
-export default class Manifest {
-  static get() {
-    const manifest = window.chrome.runtime.getManifest();
-    manifest.id = window.chrome.runtime.id;
-    return manifest;
-  }
-}
