ffe67fe783ad926673514414369d0d06377bb348
Hidetake Iwata <int128@gmail.com>
2022-06-06 10:54:14 +0900

    Delete v2 directory

--- v2/src/models/Folder.js
+++
@@ -1,9 +0,0 @@
-import { Record, Seq } from 'immutable';
-
-export default class Folder extends Record({
-  id: null,
-  title: null,
-  depth: 0,
-  items: Seq(),
-}) {
-}
--- v2/src/models/Theme.js
+++
@@ -1,7 +0,0 @@
-import { Record } from 'immutable';
-
-export default class Theme extends Record({
-  id: null,
-  title: null,
-}) {
-}
