d35149206fb2c7bc3d9d7d4254005fc86bd4e885
Hidetake Iwata <int128@gmail.com>
2022-06-09 09:55:29 +0900

    Use typescript-eslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+// https://typescript-eslint.io/docs/linting/
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'react-app',
+    'react-app/jest',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+}
