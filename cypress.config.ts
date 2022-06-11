eda97789ae31603d84ff845259b1bd82ddaeb583
Hidetake Iwata <int128@gmail.com>
2022-06-11 14:16:21 +0900

    Load extension

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,11 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('before:browser:launch', (browser, launchOptions) => {
+        // https://docs.cypress.io/api/plugins/browser-launch-api#Modify-browser-launch-arguments-preferences-and-extensions
+        launchOptions.extensions.push(`${__dirname}/build`)
+        return launchOptions
+      })
     },
   },
 });
