f7f7e695bc845110fac8a26fea435e54ff6b9981
Hidetake Iwata <int128@gmail.com>
2022-06-11 15:38:00 +0900

    Fix styles

--- cypress/component/App.cy.tsx
+++ cypress/component/App.cy.tsx
@@ -2,6 +2,7 @@ import { App } from '../../src/App'
 
 describe('<App>', () => {
   it('mounts', () => {
+    cy.viewport(1024, 576)
     cy.mount(<App />)
   })
 })
--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -21,6 +21,8 @@ import './commands'
 
 import { mount } from 'cypress/react'
 
+import '../../src/index.css'
+
 // Augment the Cypress namespace to include type definitions for
 // your custom command.
 // Alternatively, can be defined in cypress/support/component.d.ts
