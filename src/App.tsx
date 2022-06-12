047af0b83994b256213e970148220c1c0746e9da
Hidetake Iwata <int128@gmail.com>
2022-06-12 09:15:13 +0900

    Add toggles

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
+import { useState } from 'react'
 import './App.css'
 
 import Bookmarks from './Bookmarks/component'
 import Preferences from './Preferences/component'
+import { Toggles } from './Toggles/model'
 import TopSites from './TopSites/component'
 
-const App = () => (
-  <div className="App">
-    <TopSites />
-    <Bookmarks />
-    <Preferences />
-  </div>
-)
+const App = () => {
+  const [toggles, setToggles] = useState<Toggles>()
+  return (
+    <div className="App">
+      {toggles?.topSites ? <TopSites /> : null}
+      {toggles?.bookmarks ? <Bookmarks /> : null}
+      <Preferences onTogglesChange={setToggles} />
+    </div>
+  )
+}
 
 export default App
