aa3a19ab41eeb59dcd100030f3625b050ac20a95
Hidetake Iwata <int128@gmail.com>
2022-06-28 15:13:25 +0900

    Add type check

--- src/Toggles/repository.ts
+++ src/Toggles/repository.ts
@@ -5,6 +5,12 @@ export const useToggles = () =>
   useLocalStorage<Toggles>({
     key: 'v3.toggles',
     initialValue: defaultToggles,
-    parse: (stored: string) => JSON.parse(stored) as Toggles,
+    parse: (stored: string): Toggles => {
+      const p = JSON.parse(stored) as unknown
+      if (typeof p === 'object') {
+        return p as Toggles
+      }
+      throw new Error(`invalid JSON: ${stored}`)
+    },
     stringify: (value: Toggles) => JSON.stringify(value),
   })
