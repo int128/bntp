afb9bc8243b65ad1fe9338c9059c1828faed3fef
Hidetake Iwata <int128@gmail.com>
2022-06-28 16:14:31 +0900

    Change screenshot to 1200x800 (#50)
    * Change screenshot to 1920x1080
    
    * Update App.cy.tsx

--- cypress/component/App.cy.tsx
+++ cypress/component/App.cy.tsx
@@ -2,7 +2,7 @@ import App from '../../src/App'
 
 describe('<App>', () => {
   it('mounts', () => {
-    cy.viewport(1024, 576)
+    cy.viewport(1200, 800)
     cy.mount(<App />)
     cy.screenshot()
   })
