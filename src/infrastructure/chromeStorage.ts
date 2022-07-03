7987d6f61486e966aaa2a0add8039dce92a8d59a
Hidetake Iwata <int128@gmail.com>
2022-07-03 18:45:14 +0900

    Fix infinite loop

--- src/infrastructure/chromeStorage.ts
+++ src/infrastructure/chromeStorage.ts
@@ -10,20 +10,28 @@ type Spec<T> = {
 export const useChromeStorage = <T>(spec: Spec<T>): [T, (newValue: T) => void] => {
   const [storedValue, setStoredValue] = useState<T>(spec.initialValue)
 
-  useEffect(() => {
-    chrome.storage[spec.areaName].get(spec.key, (items) => {
-      if (spec.key in items) {
+  useEffect(
+    () => {
+      console.debug(`chrome.storage.${spec.areaName}.get(${spec.key})`)
+      chrome.storage[spec.areaName].get(spec.key, (items) => {
         const value = items[spec.key] as unknown
+        if (value === undefined) {
+          return
+        }
         spec.assertType(value)
         setStoredValue(value)
-      }
-    })
-    return subscribeChromeStorage(spec, setStoredValue)
-  }, [spec])
+      })
+      return subscribeChromeStorage(spec, setStoredValue)
+    },
+    // Don't set "spec" because it causes infinite loop.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [spec.key, spec.areaName]
+  )
 
   return [
     storedValue,
     (newValue: T) => {
+      console.debug(`chrome.storage.${spec.areaName}.set(${spec.key})`)
       // no need to call setStoredValue() because Chrome will trigger an event
       chrome.storage[spec.areaName].set({ [spec.key]: newValue }, undefined)
     },
@@ -33,8 +41,10 @@ export const useChromeStorage = <T>(spec: Spec<T>): [T, (newValue: T) => void] =
 const subscribeChromeStorage = <T>(spec: Spec<T>, handler: (newValue: T) => void) => {
   const listener = (changes: { [key: string]: chrome.storage.StorageChange }, areaName: chrome.storage.AreaName) => {
     if (areaName === spec.areaName && spec.key in changes) {
+      console.debug(`chrome.storage.${areaName}.onChanged(${spec.key})`)
       const newValue = changes[spec.key].newValue as unknown
       if (newValue === undefined) {
+        handler(spec.initialValue)
         return
       }
       spec.assertType(newValue)
