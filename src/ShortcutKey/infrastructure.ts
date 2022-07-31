df4ba0f676f7fd9950650f1b9f7137419daec73a
Hidetake Iwata <int128@gmail.com>
2022-07-31 14:50:27 +0900

    Handle only global key event

--- src/ShortcutKey/infrastructure.ts
+++ src/ShortcutKey/infrastructure.ts
@@ -3,10 +3,11 @@ import { useEffect } from 'react'
 export const useGlobalKey = (handler: (key: string) => void) =>
   useEffect(() => {
     const listener = (e: KeyboardEvent) => {
-      if (e.key) {
+      // prevent handling a child event such as <input>
+      if (e.target === document.body && e.key) {
         handler(e.key)
       }
     }
-    window.addEventListener('keypress', listener)
-    return () => window.removeEventListener('keypress', listener)
+    window.addEventListener('keydown', listener)
+    return () => window.removeEventListener('keydown', listener)
   })
