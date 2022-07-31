c378823a3546ed3381264ec087584f34d726c2e2
Hidetake Iwata <int128@gmail.com>
2022-07-31 16:24:19 +0900

    Use the Google Favicon Snatcher for favicon

--- src/infrastructure/favicon.ts
+++ src/infrastructure/favicon.ts
@@ -1,3 +1,8 @@
-// TODO: favicon URL is no longer supported in MV3
+// TODO: official favicon API is not yet supported
 // https://github.com/GoogleChrome/developer.chrome.com/issues/1541
-export const faviconBackgroundImage = (url: string) => `url(chrome://favicon/${url})`
+// https://bugs.chromium.org/p/chromium/issues/detail?id=104102
+export const faviconBackgroundImage = (url: string): string | undefined => {
+  if (url.startsWith('https://') || url.startsWith('http://')) {
+    return `url(https://www.google.com/s2/favicons?sz=32&domain_url=${url})`
+  }
+}
