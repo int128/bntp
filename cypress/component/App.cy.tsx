54250c952b8944b476b2274a76dcd951d9ec991f
Hidetake Iwata <int128@gmail.com>
2022-07-31 17:12:55 +0900

    refactor: assert bookmark in Cypress

--- cypress/component/App.cy.tsx
+++ cypress/component/App.cy.tsx
@@ -4,6 +4,7 @@ describe('<App>', () => {
   it('mounts', () => {
     cy.viewport(1200, 800)
     cy.mount(<App />)
+    cy.get('.Bookmark > a[href="http://www.google.com/"]')
     cy.screenshot()
   })
 })
