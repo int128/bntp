23d1fbb6d0c2c380e091de7d9947832db6e640d9
Hidetake Iwata <int128@gmail.com>
2022-08-08 22:14:36 +0900

    refactor: wait for bookmark icons (#119)
    * refactor: wait for bookmark icons
    
    * Set macbook-11

--- cypress/component/App.cy.tsx
+++ cypress/component/App.cy.tsx
@@ -2,9 +2,13 @@ import App from '../../src/App'
 
 describe('<App>', () => {
   it('mounts', () => {
-    cy.viewport(1200, 800)
+    cy.viewport('macbook-11')
     cy.mount(<App />)
     cy.get('.Bookmark > a[href="http://www.google.com/"]')
+    cy.get('.BookmarkButton__Icon').and(($img) => {
+      // https://stackoverflow.com/questions/51246606/test-loading-of-image-in-cypress
+      expect($img[0].naturalWidth).to.be.greaterThan(0)
+    })
     cy.screenshot()
   })
 })
