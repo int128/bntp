4820e21dec13548f428b72ed99fe62c22cd4c317
Hidetake Iwata <int128@gmail.com>
2022-08-08 14:25:25 +0900

    refactor: remove faviconBackgroundImage()

--- src/infrastructure/favicon.ts
+++ src/infrastructure/favicon.ts
@@ -1,12 +1,6 @@
 // TODO: official favicon API is not yet supported
 // https://github.com/GoogleChrome/developer.chrome.com/issues/1541
 // https://bugs.chromium.org/p/chromium/issues/detail?id=104102
-export const faviconBackgroundImage = (url: string): string | undefined => {
-  if (url.startsWith('https://') || url.startsWith('http://')) {
-    return `url(https://www.google.com/s2/favicons?sz=32&domain_url=${url})`
-  }
-}
-
 export const faviconImage = (url: string): string | undefined => {
   if (url.startsWith('https://') || url.startsWith('http://')) {
     return `https://www.google.com/s2/favicons?sz=32&domain_url=${url}`
