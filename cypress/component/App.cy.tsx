16a09476869398782d7927c3fdf9ccd6a3c1bdec
Hidetake Iwata <int128@gmail.com>
2022-08-10 17:31:07 +0900

    Use Google favicon on Cypress

--- cypress/component/App.cy.tsx
+++ cypress/component/App.cy.tsx
@@ -2,13 +2,22 @@ import App from '../../src/App'
 
 describe('<App>', () => {
   it('mounts', () => {
+    // favicon API is not available on Cypress
+    cy.intercept(
+      {
+        method: 'GET',
+        pathname: '/_favicon/',
+      },
+      (req) => {
+        const { pageUrl, size } = req.query
+        req.redirect(`https://www.google.com/s2/favicons?domain_url=${encodeURIComponent(pageUrl)}&sz=${size}`)
+      }
+    ).as('favicon')
+
     cy.viewport('macbook-11')
     cy.mount(<App />)
     cy.get('.Bookmark > a[href="http://www.google.com/"]')
-    cy.get('.BookmarkButton__Icon').and(($img) => {
-      // https://stackoverflow.com/questions/51246606/test-loading-of-image-in-cypress
-      expect($img[0].naturalWidth).to.be.greaterThan(0)
-    })
+    cy.wait('@favicon')
     cy.screenshot()
   })
 })
