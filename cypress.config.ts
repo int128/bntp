54ca3f44987430b3c13631650abd4f02b82686d2
Hidetake Iwata <int128@gmail.com>
2022-08-10 18:23:57 +0900

    Generate high resolution screenshot (#133)
    * Use headless Chrome
    
    * Set window size option

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,26 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress'
+
+const width = 1200
+const height = width * (9 / 16)
 
 export default defineConfig({
   component: {
     devServer: {
-      framework: "create-react-app",
-      bundler: "webpack",
+      framework: 'create-react-app',
+      bundler: 'webpack',
+    },
+    video: false,
+    viewportWidth: width,
+    viewportHeight: height,
+    setupNodeEvents(on, config) {
+      on('before:browser:launch', (browser, launchOptions) => {
+        // https://www.cypress.io/blog/2021/03/01/generate-high-resolution-videos-and-screenshots/
+        if (browser.name === 'chrome' && browser.isHeadless) {
+          launchOptions.args.push(`--window-size=${width},${height}`)
+          launchOptions.args.push('--force-device-scale-factor=2')
+        }
+        return launchOptions
+      })
     },
   },
-});
+})
--- cypress/component/App.cy.tsx
+++ cypress/component/App.cy.tsx
@@ -14,7 +14,6 @@ describe('<App>', () => {
       }
     ).as('favicon')
 
-    cy.viewport('macbook-11')
     cy.mount(<App />)
     cy.get('.Bookmark > a[href="http://www.google.com/"]')
     // https://stackoverflow.com/questions/51246606/test-loading-of-image-in-cypress
