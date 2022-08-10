8ee0b49bb0160862e69e2bc15f66f0310bae18a4
Hidetake Iwata <int128@gmail.com>
2022-08-10 19:28:54 +0900

    Revise fixture (#135)

--- cypress/fixtures/topSites.ts
+++ cypress/fixtures/topSites.ts
@@ -4,28 +4,24 @@ const topSites = [
     url: 'https://github.com/',
   },
   {
-    title: 'Facebook',
-    url: 'https://www.facebook.com/',
-  },
-  {
-    title: 'Gmail',
-    url: 'https://mail.google.com/mail/',
-  },
-  {
     title: 'Google',
     url: 'http://www.google.com/',
   },
   {
-    title: 'Google Calendar',
-    url: 'https://www.google.com/calendar/',
+    title: 'Gmail',
+    url: 'https://mail.google.com/',
   },
   {
-    title: 'AWS Management Console',
-    url: 'https://console.aws.amazon.com',
+    title: 'Google Calendar',
+    url: 'https://calendar.google.com/',
   },
   {
     title: 'Twitter',
     url: 'http://twitter.com/home',
+  },
+  {
+    title: 'Facebook',
+    url: 'https://www.facebook.com/',
   },
   {
     title: 'Slashdot',
