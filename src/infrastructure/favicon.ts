41f698d858416e14c0f9d914f866395fb65d7aca
Hidetake Iwata <int128@gmail.com>
2022-08-10 17:05:13 +0900

    Use new favicon API

--- src/infrastructure/favicon.ts
+++ src/infrastructure/favicon.ts
@@ -1,8 +1,2 @@
-// TODO: official favicon API is not yet supported
-// https://github.com/GoogleChrome/developer.chrome.com/issues/1541
-// https://bugs.chromium.org/p/chromium/issues/detail?id=104102
-export const faviconImage = (url: string): string | undefined => {
-  if (url.startsWith('https://') || url.startsWith('http://')) {
-    return `https://www.google.com/s2/favicons?sz=32&domain_url=${url}`
-  }
-}
+// https://bugs.chromium.org/p/chromium/issues/detail?id=104102#c63
+export const faviconImage = (url: string) => `/_favicon/?pageUrl=${encodeURIComponent(url)}&size=32`
