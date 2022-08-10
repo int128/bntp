592054f346850e83896d6d552915e839e38832b4
Hidetake Iwata <int128@gmail.com>
2022-08-10 10:38:58 +0900

    Prepare for production

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import App from './App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { migratePreferencesFromV2ToV3 } from './migration'
-import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
@@ -17,4 +16,4 @@ migratePreferencesFromV2ToV3().catch((e) => console.error(e))
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+// reportWebVitals()
