fa7aebd70735b5a3016e65d10c700ef902307dcf
Hidetake Iwata <int128@gmail.com>
2022-08-26 22:10:39 +0900

    Add window header to screenshots

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -14,6 +14,7 @@
 // ***********************************************************
 
 import './commands'
+import './component.css'
 import '../../src/index.css'
 import { mount } from 'cypress/react'
 
