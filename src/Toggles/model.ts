26a0454d5c0c0408fff34cd17871c9d33cd584a9
Hidetake Iwata <int128@gmail.com>
2022-08-28 09:13:43 +0900

    Refactor

--- src/Toggles/model.ts
+++ src/Toggles/model.ts
@@ -1,29 +1,29 @@
+type PropertyValidator<T> = Record<keyof T, (prop: unknown) => boolean>
+
+const validateObject = <T extends object>(o: unknown, validator: PropertyValidator<T>): o is T =>
+  typeof o === 'object' &&
+  o !== null &&
+  // object has all keys of T
+  Object.keys(validator).every((key) => Object.hasOwn(o, key)) &&
+  // all properties of object are valid
+  Object.entries(o).every(([key, prop]) => key in validator && validator[key as keyof T](prop))
+
 export type Toggles = {
   readonly topSites: boolean
   readonly bookmarks: boolean
   readonly indent: boolean
 }
 
+const togglesValidator: PropertyValidator<Toggles> = {
+  topSites: (prop) => typeof prop === 'boolean',
+  bookmarks: (prop) => typeof prop === 'boolean',
+  indent: (prop) => typeof prop === 'boolean',
+}
+
+export const isToggles = (o: unknown): o is Toggles => validateObject(o, togglesValidator)
+
 export const defaultToggles: Toggles = {
   bookmarks: true,
   topSites: true,
   indent: false,
-}
-
-const togglesValidators: Record<keyof Toggles, (v: unknown) => boolean> = {
-  topSites: (v) => typeof v === 'boolean',
-  bookmarks: (v) => typeof v === 'boolean',
-  indent: (v) => typeof v === 'boolean',
-}
-
-export const isToggles = (value: unknown): value is Toggles => validateObject(value, togglesValidators)
-
-const validateObject = <T>(value: unknown, validators: Record<string, (v: unknown) => boolean>): value is T => {
-  if (typeof value !== 'object' || value === null) {
-    return false
-  }
-  return (
-    Object.keys(validators).every((k) => Object.hasOwn(value, k)) &&
-    Object.entries(value).every(([k, v]) => k in validators && validators[k](v))
-  )
 }
