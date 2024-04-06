1c6c0ec130ff291886dd93be625517fb586b7cbe
Hidetake Iwata <int128@gmail.com>
2024-04-06 20:56:30 +0900

    Migrate to eslint flat config (#622)

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+// @ts-check
+
+import eslint from '@eslint/js'
+import reactJsxRuntime from 'eslint-plugin-react/configs/jsx-runtime.js'
+import reactRecommended from 'eslint-plugin-react/configs/recommended.js'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['.git', 'node_modules', 'dist', 'eslint.config.js'] },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  reactRecommended,
+  reactJsxRuntime,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      'sort-imports': ['error'],
+    },
+  },
+)
