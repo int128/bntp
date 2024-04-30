57d8475becb30643f6914c3f05478753850cd4e5
Hidetake Iwata <int128@gmail.com>
2024-05-01 08:54:21 +0900

    Fix storybook

--- src/App/fixtures/chrome.mock.ts
+++ src/App/fixtures/chrome.mock.ts
@@ -4,44 +4,42 @@ import { StorageAreaMock } from '../../infrastructure/chromeStorage.mock'
 import fixtureBookmarks from './bookmarks'
 import fixtureTopSites from './topSites'
 
-export const chromeMock = () => {
-  const nullEvent = {
-    addListener: () => undefined,
-    removeListener: () => undefined,
-  }
+const nullEvent = Object.freeze({
+  addListener: () => undefined,
+  removeListener: () => undefined,
+})
 
-  const bookmarks = {
-    getTree: async () => fixtureBookmarks,
-    update: async () => undefined,
-    remove: async () => undefined,
-    onChanged: nullEvent,
-    onChildrenReordered: nullEvent,
-    onCreated: nullEvent,
-    onMoved: nullEvent,
-    onRemoved: nullEvent,
-  }
+const bookmarks = Object.freeze({
+  getTree: async () => fixtureBookmarks,
+  update: async () => undefined,
+  remove: async () => undefined,
+  onChanged: nullEvent,
+  onChildrenReordered: nullEvent,
+  onCreated: nullEvent,
+  onMoved: nullEvent,
+  onRemoved: nullEvent,
+})
 
-  const topSites = {
-    get: (callback: (topSites: chrome.topSites.MostVisitedURL[]) => void) => callback(fixtureTopSites),
-  }
+const topSites = Object.freeze({
+  get: (callback: (topSites: chrome.topSites.MostVisitedURL[]) => void) => callback(fixtureTopSites),
+})
 
-  const storage = {
-    sync: new StorageAreaMock(),
-  }
+const storage = Object.freeze({
+  sync: new StorageAreaMock(),
+})
 
-  const runtime: Pick<typeof chrome.runtime, 'id' | 'getManifest'> = {
-    id: 'dummy',
-    getManifest: () => ({
-      name: 'BNTP: Bookmarks in New Tab Page',
-      version: '1.0.0',
-      manifest_version: 3,
-    }),
-  }
+const runtime = Object.freeze({
+  id: 'dummy',
+  getManifest: (): chrome.runtime.Manifest => ({
+    name: 'BNTP: Bookmarks in New Tab Page',
+    version: '1.0.0',
+    manifest_version: 3,
+  }),
+})
 
-  return {
-    bookmarks,
-    topSites,
-    storage,
-    runtime,
-  }
-}
+export const chrome = Object.freeze({
+  bookmarks,
+  topSites,
+  storage,
+  runtime,
+})
