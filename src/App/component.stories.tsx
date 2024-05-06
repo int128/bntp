0f3614ff4147748eeb779ec91450cc6229abfc54
Hidetake Iwata <int128@gmail.com>
2024-05-06 12:03:34 +0900

    Add DarkTheme story

--- src/App/component.stories.tsx
+++ src/App/component.stories.tsx
@@ -5,7 +5,7 @@ import App from './component'
 import { ChromeContext } from '../infrastructure/chrome'
 import { FaviconContext } from '../infrastructure/favicon'
 import { LocalStorageContext } from '../infrastructure/localStorage'
-import { chrome } from '../infrastructure/chrome.mock'
+import { chromeWithFixtures } from '../infrastructure/chrome.mock'
 import { googleFavicon } from '../infrastructure/favicon.mock'
 import { nullLocalStorage } from '../infrastructure/localStorage.mock'
 
@@ -18,7 +18,32 @@ type Story = StoryObj<typeof App>
 
 export const Primary: Story = {
   render: () => (
-    <ChromeContext.Provider value={chrome}>
+    <ChromeContext.Provider value={chromeWithFixtures}>
+      <FaviconContext.Provider value={googleFavicon}>
+        <LocalStorageContext.Provider value={nullLocalStorage}>
+          <App />
+        </LocalStorageContext.Provider>
+      </FaviconContext.Provider>
+    </ChromeContext.Provider>
+  ),
+}
+
+export const DarkTheme: Story = {
+  render: () => (
+    <ChromeContext.Provider
+      value={{
+        ...chromeWithFixtures,
+        storage: {
+          sync: {
+            ...chromeWithFixtures.storage.sync,
+            // eslint-disable-next-line @typescript-eslint/require-await
+            get: async () => ({
+              'v3.selectedColorScheme': 'dark',
+            }),
+          },
+        },
+      }}
+    >
       <FaviconContext.Provider value={googleFavicon}>
         <LocalStorageContext.Provider value={nullLocalStorage}>
           <App />
