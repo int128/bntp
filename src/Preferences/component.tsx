dffd068be80097d3aa725e68bdc0f669d91370fa
Hidetake Iwata <int128@gmail.com>
2024-06-23 11:21:40 +0900

    Move preferences toggle to App

--- src/Preferences/component.tsx
+++ src/Preferences/component.tsx
@@ -1,33 +1,26 @@
 import './component.css'
-import { FC, useState } from 'react'
 import DialogComponent from '../Dialog/component'
+import { FC } from 'react'
 import ManifestComponent from '../Manifest/component'
 import ThemesComponent from '../Themes/component'
 import TogglesComponent from '../Toggles/component'
 
 type PreferencesComponentProps = {
-  initialOpen?: boolean
+  open: boolean
+  onRequestClose: () => void
 }
 
-const PreferencesComponent: FC<PreferencesComponentProps> = ({ initialOpen }) => {
-  const [open, setOpen] = useState<boolean>(initialOpen ?? false)
-  return (
-    <div className="Preferences">
-      <div className="Preferences__Toggle">
-        <button onClick={() => setOpen(true)}>Preferences</button>
-      </div>
-      <DialogComponent className="Preferences" open={open} onRequestClose={() => setOpen(false)}>
-        <div className="Preferences__Form">
-          <h2>Toggles</h2>
-          <TogglesComponent />
-          <h2>Themes</h2>
-          <ThemesComponent />
-          <h2>About</h2>
-          <ManifestComponent />
-        </div>
-      </DialogComponent>
+const PreferencesComponent: FC<PreferencesComponentProps> = ({ open, onRequestClose }) => (
+  <DialogComponent className="Preferences" open={open} onRequestClose={onRequestClose}>
+    <div className="Preferences__Form">
+      <h2>Toggles</h2>
+      <TogglesComponent />
+      <h2>Themes</h2>
+      <ThemesComponent />
+      <h2>About</h2>
+      <ManifestComponent />
     </div>
-  )
-}
+  </DialogComponent>
+)
 
 export default PreferencesComponent
