65b81d284b3f930bf27cdd1dceddfe9ce0f431f1
Hidetake Iwata <int128@gmail.com>
2025-11-15 11:57:48 +0900

    Add TODO

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -11,4 +11,11 @@ const preview: Preview = {
   },
 }
 
+// TODO: Chrome Web Store requirement
+// viewport: {
+//   width: 1280,
+//   height: 800,
+//   deviceScaleFactor: 1,
+// },
+
 export default preview
