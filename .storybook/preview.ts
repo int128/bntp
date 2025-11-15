c9783d48a6ee59c6fd9e767926a8dd1d7aff739b
Hidetake Iwata <int128@gmail.com>
2025-11-15 16:16:04 +0900

    Refactor storybook and vitest config

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,15 +1,6 @@
 import type { Preview } from '@storybook/react'
 
-const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
-}
+const preview: Preview = {}
 
 // TODO: Chrome Web Store requirement
 // viewport: {
